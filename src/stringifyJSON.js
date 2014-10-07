224eb9dea42306c67be6c9bc94cdb0aa82196e4e
BethJohnson <BethJohnson3@gmail.com>
2014-10-07 09:06:44 -0500

    First pass at recursive stringifyJSON. Seems to work for regular objects, doesn't work for non-objects.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,34 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var str = "{";
+	var first = true;
+	for(var k in obj)
+	{
+		if(!first)
+		{
+			str = str + ','; 
+		}
+		else
+		{
+			first = false;
+		}
+		
+		str = str + '\"' + k + '\"' + ':' ;
+		
+		if(typeof obj[k] == "object")
+		{
+			str = str + stringifyJSON(obj[k]);
+		}
+		else if(typeof obj[k] == "string")
+		{
+			str = str + '\"' + obj[k] + '\"';
+		}
+		else
+		{
+			str = str + obj[k];
+		}
+	}
+	console.log(str);
+  return str + "}";
 };
